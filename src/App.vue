d862ef6a0d9532c736d6cae75240dc9edcb6c838
Ivan Belyanin <belyanin-2001@list.ru>
2023-09-20 20:29:28 +0400

    Update project

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,17 @@
 </template>
 
 <style>
-  
+  *{
+    box-sizing: border-box;
+  }
+  .app {
+    margin: 0;
+    padding: 0;
+
+    font-family: 'Arial', sans-serif;
+    color: white;
+
+    background: rgb(48,43,64);
+    background: linear-gradient(180deg, rgba(48,43,64,1) 0%, rgba(97,97,97,1) 100%);
+  }
 </style>
